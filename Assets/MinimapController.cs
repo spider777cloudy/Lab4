ecb74cc8b2b516283ab272147feb3151273c026a
spider777cloudy <pariket0703@gmail.com>
2024-02-03 23:30:56 -0500

    LAB4
    Lab4

---
+++ Assets/MinimapController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapController : MonoBehaviour
+{
+    [SerializeField] private Transform _player;
+    
+    void Awake()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = 
+            new Vector3(_player.position.x, transform.position.y, _player.position.z);
+    }
+}
